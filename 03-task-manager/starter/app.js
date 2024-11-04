43ed8685891902791fefd5ef6a50fdda076ea381
pwaguzi <saisishaun4@gmail.com>
2024-11-04 09:01:54 +0300

    set the starter code for the task manager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const routes = require('./routes/tasks')
+
+//middleware
+
+app.use(express.json())
+
+//routes
+app.get('/hello', (req,res)=>{
+    res.send('Task Manager app');
+    
+})
+
+app.use('/api/v1/tasks',routes)
+
+const port = 3000
+
+app.listen(port, console.log(`server is listening on port ${port}...`));
