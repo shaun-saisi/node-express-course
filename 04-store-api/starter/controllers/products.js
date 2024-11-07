03addb956babe647463266d4d236e8b8215883ee
pwaguzi <saisishaun4@gmail.com>
2024-11-07 12:52:49 +0300

    set up basic functionality

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,16 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) =>{
-    const search = 'a'
-    const products = await Product.find({
-        name: {$regex: search, $options:'i'},
-        
-        })
+  
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(7)
     res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req, res) =>{
-    const {featured, company,name} = req.query
+    const {featured, company,name, sort,fields} = req.query
     const queryObject = {} 
     if (featured){
         queryObject.featured = featured === 'true'? true:false
@@ -21,9 +21,30 @@ const getAllProducts = async (req, res) =>{
     if(name){
         queryObject.name = name
     }
-    console.log(queryObject);
+    //console.log(queryObject);
     
-    const products = await Product.find(queryObject);
+    let result =  Product.find(queryObject)
+
+    //sort
+    if(sort){
+       const sortList = sort.split(',').join(' ');
+       result = result.sort(sortList)   
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+
+    if (fields){
+        const fieldsList = fields.split(',').join(' ');
+       result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+
+    result = result.skip(skip).limit(limit)
+    const products = await result
     res.status(200).json({products, nbHits:products.length})
 }
 
